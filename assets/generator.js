5e00f9d0081185f5b3074200332dc56ea47d4691
EMC <edwardcoad.ec@gmail.com>
2019-11-12 10:11:14 +1030

    Print to textarea complete, validations still to come

--- assets/generator.js
+++ assets/generator.js
@@ -4,6 +4,8 @@ function copytext() {
 
     /*Select the text field*/
     copyText.select();
+    
+    //Sets the selection range for mobile devices
     copyText.setSelectionRange(0,99999);
 
     document.execCommand("copy");
@@ -19,42 +21,49 @@ function passwordGenerator() {
     //Merge all character arrays into one
     //Randomly print out the array in the text field
 
-    emptyArray = []
-
+    passwordArray = []
     
-    alert("The following prompts will make up the core of your new password")
+    alert("The following prompts will make up the core of your new password, the prompts will continue until the password is at least 8 characters long.");
 
-    var sC = prompt("Please input a special character:");
+    //Continues to ask the user for input until the password is at least 8 characters long. 7 is used so that it doesn't repeat itself a third time, if only one character is input at a time
+    while (passwordArray.length<=7) {
+        var sC = prompt("Please input a special character:");
 
-    var sC = (sC.match(/.{1}/g));
-    console.log(sC);
+        var sC = (sC.match(/.{1}/g));
+        console.log(sC);
 
-    var nC = prompt("Please input a number:");
+        var nC = prompt("Please input a number:");
 
-    var nC = (nC.match(/.{1}/g));
-    console.log(nC);
+        var nC = (nC.match(/.{1}/g));
+        console.log(nC);
 
-    var lC = prompt("Please input a lowercase character:");
+        var lC = prompt("Please input a lowercase character:");
 
-    var lC = (lC.match(/.{1}/g));
-    console.log(lC);
+        var lC = (lC.match(/.{1}/g));
+        console.log(lC);
 
-    var uC = prompt("Please input an uppercase character:");
+        var uC = prompt("Please input an uppercase character:");
 
-    var uC = (uC.match(/.{1}/g));
-    console.log(uC);
+        var uC = (uC.match(/.{1}/g));
+        console.log(uC);
 
-    var passwordArray = emptyArray.concat(sC, nC, lC, uC); 
+        var passwordArray = passwordArray.concat(sC, nC, lC, uC); 
 
-    console.log(passwordArray);
+        console.log(passwordArray);
+    }
 
+    //Uses the shuffleArray to create a truely random password
     shuffleArray(passwordArray);
 
     console.log(passwordArray)
+
+    //Prints the newly generated password to the textarea
+    var textarea = document.getElementById("password");
+    textarea.value = passwordArray.join("");
 }
 
+//Randomise array element order in-pace using Durstenfeld shuffle algorithm
 function shuffleArray(array) {
-    //Randomise array element order in-pace using Durstenfeld shuffle algorithm
     
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -63,3 +72,5 @@ function shuffleArray(array) {
         array[j] = temp;
     }
 }
+
+//Validation functions
